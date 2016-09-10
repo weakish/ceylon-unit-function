7b40fce6b81b1064ac1f6fb21da7270f811603e6
Jakukyo Friel <weakish@gmail.com>
2016-09-10 22:33:22 +0800

    Fix resources invisible caused by jekyll.

---
+++ api/resources/index.js
@@ -0,0 +1,8 @@
+var index = [
+{'name': 'io.github.weakish.unit', 'type': 'package', 'url': 'index.html#section-package', 'doc': '<p>This package provides the <code>unit</code> object.\n<code>unit</code> extends the abstract class Unit,\nso we do not need to write \\Iunit.</p>', 'tags': [],'icons': ['icon-package']},
+{'name': 'Unit', 'type': 'class', 'url': 'Unit.type.html', 'doc': '<p>To avoid ugly \\Iunit.</p>', 'tags': [],'icons': ['icon-class', 'icon-decoration-abstract', 'icon-decoration-enumerated']},
+{'name': 'unit', 'type': 'object', 'url': 'unit.object.html', 'doc': '<p>Mainly used in <code>return unit;</code>.</p>', 'tags': [],'icons': ['icon-object']},
+{'name': 'unit', 'type': 'value', 'url': 'index.html#unit', 'doc': '<p>Mainly used in <code>return unit;</code>.</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'io.github.weakish.unit', 'type': 'module', 'url': 'index.html', 'doc': '<p>This module provides the <code>Unit</code> function return type.</p>', 'tags': [],'icons': ['icon-module']}];
+var tagIndex = [
+];
